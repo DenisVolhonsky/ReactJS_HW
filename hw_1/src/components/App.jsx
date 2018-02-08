dbbf70ad81aba5656c59c6db26976c2e9063b1bf
DenisVolhonsky <dvolhonsky@gmail.com>
2018-02-08 12:43:27 +0200

    creating content

---
+++ hw_1/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <h2>hi</h2>
+            </div>
+
+
+        );
+    }
+}
+
+export default App;
