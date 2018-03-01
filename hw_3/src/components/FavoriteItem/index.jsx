08af5b3be872e94c018ca9019b04e445724ac525
DenisVolhonsky <dvolhonsky@gmail.com>
2018-03-01 15:26:10 +0200

    fixing fonts

--- hw_3/src/components/FavoriteItem/index.jsx
+++ hw_3/src/components/FavoriteItem/index.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 import './style.css';
 
 const FavoriteItem = ({id, title, release_date, vote_average, poster_path, onClickDel}) => {
-    // console.log(typeof release_date, release_date );
-    // let release = release_date.substr(0,4);
     return (
         <div className="FavoriteItem">
             <img className="FavoriteItem__img" src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt=""/>
             <div className="FavoriteItem__body">
                 <h3 className="FavoriteItem__title">{title}</h3>
-                <div className="FavoriteItem__release">Release date: {release_date.substr(0,4)}</div>
-                <div className="FavoriteItem__vote">Rating: {vote_average}</div>
+                <p className="FavoriteItem__release">Release date: {release_date.substr(0,4)}</p>
+                <p className="FavoriteItem__vote">Rating: {vote_average}</p>
                 <button className="FavoriteItem__btn" onClick={()=>onClickDel(id)}>-</button>
             </div>
         </div>
