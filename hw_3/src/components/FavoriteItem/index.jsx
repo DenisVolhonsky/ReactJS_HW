e6a2c1de7a1d8a53c469ed6503fd599e9bf2e701
DenisVolhonsky <dvolhonsky@gmail.com>
2018-03-01 13:38:39 +0200

    build1

--- hw_3/src/components/FavoriteItem/index.jsx
+++ hw_3/src/components/FavoriteItem/index.jsx
@@ -7,7 +7,7 @@ const FavoriteItem = ({id, title, release_date, vote_average, poster_path, onCli
                 <img className="FavoriteItem__img" src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt=""/>
                 <div className="FavoriteItem__body">
                     <h3 className="FavoriteItem__title">{title}</h3>
-                    <p className="FavoriteItem__release">Released: {release_date.slice(0,4)}</p>
+                    <p className="FavoriteItem__release">Release date: {release_date.slice(0,4)}</p>
                     <p className="FavoriteItem__vote">Rating: {vote_average}</p>
                     <button className="FavoriteItem__btn" onClick={()=>onClickDel(id)}>-</button>
                 </div>
