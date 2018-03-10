6d7de529d04f24d9fc5e1c787640e56de2209cee
DenisVolhonsky <dvolhonsky@gmail.com>
2018-03-10 21:34:36 +0200

    chahge all stylyles

--- FilmGalleryRouting/src/components/Header/index.jsx
+++ FilmGalleryRouting/src/components/Header/index.jsx
@@ -3,11 +3,11 @@ import './style.css';
 
 const Header = () => (
     <header className="Header">
-        <h1><a className="Header__logo" href="">Home Cinema</a></h1>
+        <h1><a className="Header__logo" href="">Movie Mate</a></h1>
         <ul className="Navigation">
+            <li className="Navigation__item"><a className="Navigation__link">Home</a></li>
+            <li className="Navigation__item"><a className="Navigation__link">Movies</a></li>
             <li className="Navigation__item"><a className="Navigation__link">About</a></li>
-            <li className="Navigation__item"><a className="Navigation__link">Browse</a></li>
-            <li className="Navigation__item"><a className="Navigation__link">Login</a></li>
         </ul>
     </header>
 );
--- FilmGalleryRouting/src/components/SearchCategory/index.jsx
+++ FilmGalleryRouting/src/components/SearchCategory/index.jsx
@@ -4,7 +4,7 @@ import './style.css'
 const SearchCategory = ({onChangeCategory})=>(
                 <div className="SearchCategory">
                     <button className="SearchCategory__btn" onClick={() => onChangeCategory('popular')}>Popular</button>
-                    <button className="SearchCategory__btn" onClick={() => onChangeCategory('top_rated')}>Top rated</button>
+                    <button className="SearchCategory__btn" onClick={() => onChangeCategory('top_rated')}>Top_rated</button>
                     <button className="SearchCategory__btn" onClick={() => onChangeCategory('upcoming')}>Upcoming</button>
                 </div>
 )
